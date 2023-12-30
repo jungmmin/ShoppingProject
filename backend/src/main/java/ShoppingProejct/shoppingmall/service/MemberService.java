770a43d81a13184e31809f4839a88e396667c872
정민 <ilb0919497@gmail.com>
2023-12-30 17:57:07 +0900

    feat: gitignore 추가

--- backend/src/main/java/ShoppingProejct/shoppingmall/service/MemberService.java
+++ backend/src/main/java/ShoppingProejct/shoppingmall/service/MemberService.java
@@ -3,6 +3,9 @@ package ShoppingProejct.shoppingmall.service;
 import ShoppingProejct.shoppingmall.Domain.Member.MemberCreateDto;
 import ShoppingProejct.shoppingmall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional(readOnly = true)
 public class MemberService {
 
@@ -18,10 +22,16 @@ public class MemberService {
 
     // 회원 생성
     @Transactional
-    public void saveMember(MemberCreateDto memberCreateDto){
+    public void saveMember(MemberCreateDto memberCreateDto) {
+
+        // 비밀번호 암호화 후 저장
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        memberCreateDto.setPassword(encoder.encode(memberCreateDto.getPassword()));
+//
+//        BCryptPasswordEncoder encoder2 = new BCryptPasswordEncoder();
+//        log.info("비밀번호 무결성 = {}", encoder2.matches(encoder.encode(memberCreateDto.getPassword()), memberCreateDto.getPassword()));
         memberRepository.save(memberCreateDto.toEntity());
     }
-
 
 
 }
