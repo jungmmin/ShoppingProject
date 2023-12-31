a64b14e63ec3c17a431cd8c3b8e643e5f2a1aeed
정민 <ilb0919497@gmail.com>
2023-12-31 11:39:44 +0900

    fix: Spring cors 문제 발생 수정

--- backend/src/main/java/ShoppingProejct/shoppingmall/Security/SecurityConfig.java
+++ backend/src/main/java/ShoppingProejct/shoppingmall/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ShoppingProejct.shoppingmall.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +9,12 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -16,11 +23,24 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(HttpBasicConfigurer::disable);
         http.csrf(CsrfConfigurer::disable); // 외부 POST 요청을 받아야하니 csrf는 꺼준다.
-        http.cors(Customizer.withDefaults()); // CORS를 커스텀하려면 이렇게
+        // CORS 설정
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L); //1시간
+                return config;
+            }
+        })); // CORS를 커스텀하려면 이렇게
         http.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/**").permitAll().anyRequest().authenticated());
 
         return http.build();
     }
+
 }
--- backend/src/main/java/ShoppingProejct/shoppingmall/config/CommonConfig.java
+++ backend/src/main/java/ShoppingProejct/shoppingmall/config/CommonConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CommonConfig {
@@ -13,4 +19,6 @@ public class CommonConfig {
     public PasswordEncoder encodePassword(){
         return new BCryptPasswordEncoder();
     }
+
+
 }
